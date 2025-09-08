796db5a0e8e650eaf2385a6821a37d0e5dcb1348
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-09-08 05:27:37 -0500

    delete archive.txt

--- ejercicio_pyspark/ETL_Superstore/etl_superstore.py
+++ ejercicio_pyspark/ETL_Superstore/etl_superstore.py
@@ -1,19 +1,9 @@
 from pyspark.sql import SparkSession
-<<<<<<< HEAD
 from pyspark.sql.functions import col, to_date, sum as pyspark_sum, avg
 
 spark = SparkSession.builder.appName("ETL_Superstore").getOrCreate()
 
 df = spark.read.csv("C:/taller_1_scrapy/ejercicio_pyspark/ETL_Superstore/Sample - Superstore.csv",header=True,inferSchema=True)
-=======
-from pyspark.sql.functions import col, to_date, sum as _sum
-
-spark = SparkSession.builder \
-    .appName("ETL_Superstore") \
-    .getOrCreate()
-    
-df = spark.read.csv("Superstore.csv",header=True,inferSchema=True)
->>>>>>> c34afd9eb9dfdb4f0700e6f4ac0b3d9059c4d18e
 
 df.show(5)
 
@@ -21,7 +11,6 @@ df.printSchema()
 
 df = df.withColumn("Sales",col("Sales").cast("double"))
 df = df.withColumn("Profit",col("Profit").cast("double"))
-<<<<<<< HEAD
 
 df = df.withColumn("Order Date",to_date(col("Order Date"),"MM/dd/yy"))
 
@@ -34,23 +23,3 @@ gasto_cliente = df.groupBy("Customer ID","Customer Name").agg(pyspark_sum("Sales
 ventas_categoria.coalesce(1).write.csv("out/ventas_categoria.csv",header=True,mode="overwrite")
 ventas_categoria.coalesce(1).write.csv("out/utilidad_region.csv",header=True,mode="overwrite")
 ventas_categoria.coalesce(1).write.csv("out/gasto_cliente.csv",header=True,mode="overwrite")
-=======
-df = df.withColumn("Order Date",to_date(col("Order Date"),"MM/dd/yyyy"))
-
-ventas_categoria = df.groupBy("Category").sum("Sales").withColumnRenamed("sum(Sales)","Total_Sales")
-ventas_categoria.show()
-
-utilidad_region = df.groupBy("Region").avg("Profit").withColumnRenamed("avg(Profit)","Avg_Profit")
-utilidad_region.show()
-
-gasto_cliente = df.groupBy("Customer Name") \
-    .agg(_sum("Sales").alias("Total_Spend")) \
-    .orderBy(col("Total_Spend").desc()) \
-        .limit(1)
-
-gasto_cliente.show()
-
-ventas_categoria.write.csv("out/ventas_categoria.csv",header=True,mode="overwrite")
-ventas_categoria.write.csv("out/utilidad_region.csv",header=True,mode="overwrite")
-ventas_categoria.write.csv("out/gasto_cliente.csv",header=True,mode="overwrite")
->>>>>>> c34afd9eb9dfdb4f0700e6f4ac0b3d9059c4d18e
