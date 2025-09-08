7a40ca9c401710ac6eb1a52cd79057d5ceb09ff0
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-09-08 06:09:16 -0500

    update superstore

--- ejercicio_pyspark/ETL_Superstore/etl_superstore.py
+++ ejercicio_pyspark/ETL_Superstore/etl_superstore.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date, sum as pyspark_sum, avg
+import os 
+import shutil 
 
 spark = SparkSession.builder.appName("ETL_Superstore").getOrCreate()
 
@@ -20,6 +22,20 @@ utilidad_region = df.groupBy("Region").agg(avg("Profit").alias("Avg_Profit"))
 
 gasto_cliente = df.groupBy("Customer ID","Customer Name").agg(pyspark_sum("Sales").alias("Total_Spent")).orderBy(col("Total_Spent").desc()).limit(1)
 
-ventas_categoria.coalesce(1).write.csv("out/ventas_categoria.csv",header=True,mode="overwrite")
-ventas_categoria.coalesce(1).write.csv("out/utilidad_region.csv",header=True,mode="overwrite")
-ventas_categoria.coalesce(1).write.csv("out/gasto_cliente.csv",header=True,mode="overwrite")
+def save_single_csv(df,final_path):
+    temp_path = final_path + "_temp"
+    
+    df.coalesce(1).write.csv(temp_path,header=True,mode="overwrite")
+    
+    for file_name in os.listdir(temp_path):
+        if file_name.startswith("part-") and file_name.endswith(".csv"):
+            shutil.move(os.path.join(temp_path,file_name),final_path)
+        
+    shutil.rmtree(temp_path)
+    
+if not os.path.exists("out"):
+    os.makedirs("out")
+
+save_single_csv(ventas_categoria,"out/ventas_categoria.csv")
+save_single_csv(ventas_categoria,"out/utilidad_region.csv")
+save_single_csv(ventas_categoria,"out/gasto_cliente.csv")
