d08cd3ceefa466f73aee396184c8d2d458730029
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-09-08 05:12:23 -0500

    create ventas_category.csv

--- ejercicio_pyspark/ETL_Superstore/etl_superstore.py
+++ ejercicio_pyspark/ETL_Superstore/etl_superstore.py
@@ -1,11 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, to_date, sum as _sum
+from pyspark.sql.functions import col, to_date, sum as pyspark_sum, avg
 
-spark = SparkSession.builder \
-    .appName("ETL_Superstore") \
-    .getOrCreate()
-    
-df = spark.read.csv("Superstore.csv",header=True,inferSchema=True)
+spark = SparkSession.builder.appName("ETL_Superstore").getOrCreate()
+
+df = spark.read.csv("C:/taller_1_scrapy/ejercicio_pyspark/ETL_Superstore/Sample - Superstore.csv",header=True,inferSchema=True)
 
 df.show(5)
 
@@ -13,21 +11,15 @@ df.printSchema()
 
 df = df.withColumn("Sales",col("Sales").cast("double"))
 df = df.withColumn("Profit",col("Profit").cast("double"))
-df = df.withColumn("Order Date",to_date(col("Order Date"),"MM/dd/yyyy"))
 
-ventas_categoria = df.groupBy("Category").sum("Sales").withColumnRenamed("sum(Sales)","Total_Sales")
-ventas_categoria.show()
+df = df.withColumn("Order Date",to_date(col("Order Date"),"MM/dd/yy"))
 
-utilidad_region = df.groupBy("Region").avg("Profit").withColumnRenamed("avg(Profit)","Avg_Profit")
-utilidad_region.show()
+ventas_categoria = df.groupBy("Category").agg(pyspark_sum("Sales").alias("Total Sales"))
 
-gasto_cliente = df.groupBy("Customer Name") \
-    .agg(_sum("Sales").alias("Total_Spend")) \
-    .orderBy(col("Total_Spend").desc()) \
-        .limit(1)
+utilidad_region = df.groupBy("Region").agg(avg("Profit").alias("Avg_Profit"))
 
-gasto_cliente.show()
+gasto_cliente = df.groupBy("Customer ID","Customer Name").agg(pyspark_sum("Sales").alias("Total_Spent")).orderBy(col("Total_Spent").desc()).limit(1)
 
-ventas_categoria.write.csv("out/ventas_categoria.csv",header=True,mode="overwrite")
-ventas_categoria.write.csv("out/utilidad_region.csv",header=True,mode="overwrite")
-ventas_categoria.write.csv("out/gasto_cliente.csv",header=True,mode="overwrite")
+ventas_categoria.coalesce(1).write.csv("out/ventas_categoria.csv",header=True,mode="overwrite")
+ventas_categoria.coalesce(1).write.csv("out/utilidad_region.csv",header=True,mode="overwrite")
+ventas_categoria.coalesce(1).write.csv("out/gasto_cliente.csv",header=True,mode="overwrite")
